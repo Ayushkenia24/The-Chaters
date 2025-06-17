21e69a195c6b9d72ef27f4d5ee1512dccb3344b5
Ayush kenia <143783762+Ayush17734@users.noreply.github.com>
2025-06-17 19:21:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Socket from './components/Socket'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Socket/>
+       </div>
+    </>
+  )
+}
+
+export default App
